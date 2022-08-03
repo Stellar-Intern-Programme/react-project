72463bd7b99adbe5fd0bac0b7de28eceb08cbdd6
EmiTirnovan <tirnovan.emilian2004@gmail.com>
2022-08-03 16:13:42 +0300

    terminat, cred

--- src/components/Slots/Slots.jsx
+++ src/components/Slots/Slots.jsx
@@ -1,3 +1,16 @@
+import "./slots.css"
+import Games from "./games"
 export function Slots(){
-    return (<></>)
+    return (<>
+    <div className="slots">
+        <Games x={"Jocuri noi"} y={12} img={"https://th.bing.com/th/id/R.bb51550d602255695ab2c0af16e87fcd?rik=Sh8h18Vyq%2f6GjQ&pid=ImgRaw&r=0"} />
+        <Games x={"Jocuri noi"} y={12} img={"https://th.bing.com/th/id/R.bb51550d602255695ab2c0af16e87fcd?rik=Sh8h18Vyq%2f6GjQ&pid=ImgRaw&r=0"} />
+        <Games x={"Jocuri noi"} y={12} img={"https://th.bing.com/th/id/R.bb51550d602255695ab2c0af16e87fcd?rik=Sh8h18Vyq%2f6GjQ&pid=ImgRaw&r=0"} />
+        <Games x={"Jocuri noi"} y={12} img={"https://th.bing.com/th/id/R.bb51550d602255695ab2c0af16e87fcd?rik=Sh8h18Vyq%2f6GjQ&pid=ImgRaw&r=0"} />
+        <Games x={"Jocuri noi"} y={12} img={"https://th.bing.com/th/id/R.bb51550d602255695ab2c0af16e87fcd?rik=Sh8h18Vyq%2f6GjQ&pid=ImgRaw&r=0"} />
+        <Games x={"Jocuri noi"} y={12} img={"https://th.bing.com/th/id/R.bb51550d602255695ab2c0af16e87fcd?rik=Sh8h18Vyq%2f6GjQ&pid=ImgRaw&r=0"} />
+        <Games x={"Jocuri noi"} y={12} img={"https://th.bing.com/th/id/R.bb51550d602255695ab2c0af16e87fcd?rik=Sh8h18Vyq%2f6GjQ&pid=ImgRaw&r=0"} />
+        
+    </div>
+    </>)
 }
