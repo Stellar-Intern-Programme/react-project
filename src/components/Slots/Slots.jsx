ef5a775694a9544b6422b08d3cdaca80dc55a32d
EmiTirnovan <tirnovan.emilian2004@gmail.com>
2022-08-08 14:11:48 +0300

    versiune finala

--- src/components/Slots/Slots.jsx
+++ src/components/Slots/Slots.jsx
@@ -4,12 +4,12 @@ export function Slots(){
     return (<>
     <div className="slots">
         <Games x={"Jocuri noi"} y={12} img={"https://th.bing.com/th/id/R.bb51550d602255695ab2c0af16e87fcd?rik=Sh8h18Vyq%2f6GjQ&pid=ImgRaw&r=0"} />
-        <Games x={"Jocuri noi"} y={12} img={"https://th.bing.com/th/id/R.bb51550d602255695ab2c0af16e87fcd?rik=Sh8h18Vyq%2f6GjQ&pid=ImgRaw&r=0"} />
-        <Games x={"Jocuri noi"} y={12} img={"https://th.bing.com/th/id/R.bb51550d602255695ab2c0af16e87fcd?rik=Sh8h18Vyq%2f6GjQ&pid=ImgRaw&r=0"} />
-        <Games x={"Jocuri noi"} y={12} img={"https://th.bing.com/th/id/R.bb51550d602255695ab2c0af16e87fcd?rik=Sh8h18Vyq%2f6GjQ&pid=ImgRaw&r=0"} />
-        <Games x={"Jocuri noi"} y={12} img={"https://th.bing.com/th/id/R.bb51550d602255695ab2c0af16e87fcd?rik=Sh8h18Vyq%2f6GjQ&pid=ImgRaw&r=0"} />
-        <Games x={"Jocuri noi"} y={12} img={"https://th.bing.com/th/id/R.bb51550d602255695ab2c0af16e87fcd?rik=Sh8h18Vyq%2f6GjQ&pid=ImgRaw&r=0"} />
-        <Games x={"Jocuri noi"} y={12} img={"https://th.bing.com/th/id/R.bb51550d602255695ab2c0af16e87fcd?rik=Sh8h18Vyq%2f6GjQ&pid=ImgRaw&r=0"} />
+        <Games x={"Jocuri vechi"} y={10} img={"https://th.bing.com/th/id/R.bb51550d602255695ab2c0af16e87fcd?rik=Sh8h18Vyq%2f6GjQ&pid=ImgRaw&r=0"} />
+        <Games x={"Jocuri naspa"} y={14} img={"https://th.bing.com/th/id/R.bb51550d602255695ab2c0af16e87fcd?rik=Sh8h18Vyq%2f6GjQ&pid=ImgRaw&r=0"} />
+        <Games x={"Jocuri horror"} y={9} img={"https://th.bing.com/th/id/R.bb51550d602255695ab2c0af16e87fcd?rik=Sh8h18Vyq%2f6GjQ&pid=ImgRaw&r=0"} />
+        <Games x={"Jocuri smechere"} y={12} img={"https://th.bing.com/th/id/R.bb51550d602255695ab2c0af16e87fcd?rik=Sh8h18Vyq%2f6GjQ&pid=ImgRaw&r=0"} />
+        <Games x={"Jocuri frumoase, dar nu la fel de frumoase ca tine"} y={12} img={"https://th.bing.com/th/id/R.bb51550d602255695ab2c0af16e87fcd?rik=Sh8h18Vyq%2f6GjQ&pid=ImgRaw&r=0"} />
+        <Games x={"Jocuri de jucat"} y={13} img={"https://th.bing.com/th/id/R.bb51550d602255695ab2c0af16e87fcd?rik=Sh8h18Vyq%2f6GjQ&pid=ImgRaw&r=0"} />
         
     </div>
     </>)
